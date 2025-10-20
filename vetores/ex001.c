9b864d81810fa3f6fb6fc9e34916bcff1c0a5d62
Rafael Maciel <maciel.rafael2005@gmail.com>
2025-10-20 08:15:00 -0300

    Pronto

--- vetores/ex001.c
+++ vetores/ex001.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main(){
+
+    int i, vet[6];
+
+    for(i=0; i<6; i++){
+
+        printf("Digite seu número: ");
+        scanf("%d", &vet[i]);
+
+    }
+
+    for(i=0; i<6; i++){
+
+        printf("%d\n", vet[i]);
+
+    }
+
+    return 0;
+}
