e878d07df23273937627240907bd1dfcdf95cdb7
Rafael Maciel <maciel.rafael2005@gmail.com>
2025-10-20 08:17:32 -0300

    pronto

--- vetores/ex002.c
+++ vetores/ex002.c
@@ -1,3 +1,5 @@
+//Faça um programa que peça 6 valores inteiros e, depois, apresente-os na tela.
+
 #include <stdio.h>
 
 int main(){
@@ -8,10 +10,14 @@ int main(){
 
         printf("Digite seu número: ");
         scanf("%d", &vet[i]);
+
+    }
+
+    for(i=0; i<6; i++){
+
         printf("%d\n", vet[i]);
 
     }
-  
 
     return 0;
 }
